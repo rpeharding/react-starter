2eb039c5a34ed6b7e97af9937c21fbcb18492376
rpeharding <139493134+rpeharding@users.noreply.github.com>
2024-01-29 10:48:00 +0000

    adding crypto api

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -11,18 +11,21 @@ class Child extends Component {
 
   async componentDidMount() {
     console.log("HTML is now inside the DOM");
-
-    const { data } = await axios.get(
-      `https://thesimpsonsquoteapi.glitch.me/quotes`
-    );
-    this.setState({ simpsons: data });
+    try {
+      const { data } = await axios.get(
+        `https://api.coingecko.com/api/v3/coins/list?x_cg_demo_api_key=CG-7kXqTFkJKhp4ZPBs5XfJEYBn`
+      );
+      this.setState({ cyrpto: data });
+    } catch (err) {
+      console.log("error with api");
+    }
   }
 
   render() {
     console.log("render ran, HTML getting created", this.state);
 
-    if (this.state.simpsons) {
-      return <p>We have data</p>
+    if (this.state.crypto) {
+      return <p>We have data</p>;
     }
 
     return <p>Loading.....</p>;
